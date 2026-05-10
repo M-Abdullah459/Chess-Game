673cc6e4cd98a3e1b65e780cbe60bb7a3ca110e2
M-Abdullah459 <m.abdullah17532@gmail.com>
2026-05-10 14:23:09 +0500

    Bishop movement update

--- Chess game/Bishop.h
+++ Chess game/Bishop.h
@@ -4,6 +4,7 @@ class Bishop :public Piece
 {
 
 public:
+
 	Bishop(char name, bool isWhite, int row, int column) : Piece(isWhite, row, column, name) {}
 	bool isValidMove(int endRow, int endColumn)override {
 		if ((position.row != endRow && position.column != endColumn) && (abs(endRow - position.row) == abs(endColumn - position.column)))
