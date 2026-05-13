11c6ca959e8f3dc0606ba08bc179e83a9e8a1eb1
Ahmad Waqas <wk3411086@gmail.com>
2026-05-13 09:58:09 +0500

    Implemented Bishop class with diagonal move validation

--- Chess game/Bishop.h
+++ Chess game/Bishop.h
@@ -4,9 +4,11 @@ class Bishop :public Piece
 {
 
 public:
-
+	// Constructor to initialize bishop position and color
 	Bishop(char name, bool isWhite, int row, int column) : Piece(isWhite, row, column, name) {}
+	// Checks if bishop move is valid
 	bool isValidMove(int endRow, int endColumn)override {
+		// Bishop moves diagonally
 		if ((position.row != endRow && position.column != endColumn) && (abs(endRow - position.row) == abs(endColumn - position.column)))
 			return true;
 		return false;
