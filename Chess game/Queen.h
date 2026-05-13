266d027e63bd9aef22f07c14e1d7ade56f6db1b5
Ahmad Waqas <wk3411086@gmail.com>
2026-05-13 09:45:32 +0500

    Added Queen header file with move validation

--- Chess game/Queen.h
+++ Chess game/Queen.h
@@ -1,13 +1,19 @@
 #pragma once
 #include"Piece.h"
+// Queen class derived from Piece
 class Queen :public Piece
 {
 	char name;
 public:
+	// Constructor to initialize queen data
 	Queen(char name, bool isWhite, int row, int column) :Piece(isWhite, row, column, name) {}
+	// Checks whether the move is valid for Queen
 	bool isValidMove(int endRow, int endColumn) override {
+
+		// Horizontal or vertical movement
 		if ((position.row == endRow && position.column != endColumn) || (position.row != endRow && position.column == endColumn))
 			return true;
+		// Diagonal movement
 		if ((position.row != endRow && position.column != endColumn) && (abs(position.row - endRow) == abs(position.column - endColumn)))
 			return true;
 		return false;
