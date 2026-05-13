f7b07fc200723c45337c489c9e15fabce03485c5
Ahmad Waqas <wk3411086@gmail.com>
2026-05-13 10:04:49 +0500

    Implemented Knight class with L-shape move validation

--- Chess game/Knight.h
+++ Chess game/Knight.h
@@ -4,8 +4,11 @@ class Knight :public Piece
 {
 	char name;
 public:
+	// Constructor to initialize knight data
 	Knight(char name, bool isWhite, int row, int column) :Piece(isWhite, row, column, name) {}
+	// Checks whether knight move is valid
 	bool isValidMove(int endRow, int endColumn) override {
+		// Knight moves in L-shape
 		if ((abs(position.row - endRow) == 2 && abs(position.column - endColumn) == 1) || (abs(position.row - endRow) == 1 && abs(position.column - endColumn) == 2))
 			return true;
 		return false;
