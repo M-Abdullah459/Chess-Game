8c031177ec280e6c6cb8bbea84bad657010ff08f
Ahmad Waqas <wk3411086@gmail.com>
2026-05-13 10:28:40 +0500

    Implemented King class with one-step movement validation in all directions

--- Chess game/King.h
+++ Chess game/King.h
@@ -4,17 +4,22 @@ class King :public Piece
 {
 	char name;
 public:
+	// Constructor to initialize king
 	King(char name, bool isWhite, int row, int column) :Piece(isWhite, row, column, name) {}
+	// Checks valid move for king (one step in any direction)
 	bool isValidMove(int endRow, int endColumn) override {
+		// Horizontal move (1 step)
 		if (position.row == endRow && position.column != endColumn) {
 			if (abs(endColumn - position.column) == 1)
 				return true;
 		}
+		// Vertical move (1 step)
 		else if (position.row != endRow && position.column == endColumn) {
 			if (abs(endRow - position.row) == 1) {
 				return true;
 			}
 		}
+		// Diagonal move (1 step)
 		if (position.row != endRow && position.column != endColumn) {
 			if ((abs(position.row - endRow) == 1) && abs(position.column - endColumn) == 1) {
 				return true;
